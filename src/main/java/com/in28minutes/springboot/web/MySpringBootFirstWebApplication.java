4362f179411b2e619d0ad9c68b649ca8b5a852cf
surasak <surasak@DESKTOP-SACR9OC>
2022-02-26 21:05:43 +0700

    commit

---
+++ src/main/java/com/in28minutes/springboot/web/MySpringBootFirstWebApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootFirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootFirstWebApplication.class, args);
+	}
+
+}
